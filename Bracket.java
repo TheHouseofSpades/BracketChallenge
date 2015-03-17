b6addaff4bef1d35c960638aa069697c511b19b9
TheHouseofSpades <s544506@011-LC26-284808.local>
2015-03-17 14:00:45 -0500

    Everything in

---
+++ Bracket.java
@@ -0,0 +1,70 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+public class Bracket{
+  
+ Team teams[] = new Team[16];
+ 
+ 
+ public void Bracket() throws IOException{
+   Scanner fileReader =  new Scanner(new File("east.txt"));
+   fileReader.nextLine();
+   for(int i=0;i<16;i++){
+    int rank = fileReader.nextInt();
+    String teamName = fileReader.next();
+    int wins = fileReader.nextInt();
+    int losses = fileReader.nextInt();
+    double pyth = fileReader.nextDouble();
+    double adjO = fileReader.nextDouble();
+    double adjORank = fileReader.nextDouble();
+    double adjD = fileReader.nextDouble();
+    double adjDRank = fileReader.nextDouble();
+    double adjT = fileReader.nextDouble();
+    double adjTRank = fileReader.nextDouble();
+    double luckRank = fileReader.nextDouble();
+    double sosPyth = fileReader.nextDouble();
+    double sosPythRank = fileReader.nextDouble();
+    double oppO = fileReader.nextDouble();
+    double oppORank = fileReader.nextDouble();
+    double oppD = fileReader.nextDouble();
+    double oppDRank = fileReader.nextDouble();
+    double ncsosPyth = fileReader.nextDouble();
+    double ncsosPythRank = fileReader.nextDouble();
+    fileReader.nextLine();
+    teams[i] = new Team(rank, teamName, wins, losses, pyth, adjO, adjORank, adjD, adjDRank, adjT, adjTRank, luckRank, sosPyth, sosPythRank, oppO, oppORank, oppD, oppDRank, ncsosPyth, ncsosPythRank);
+   }
+ }
+ public String toString(){
+   for(Team t: teams)
+    return(t.rank + ". " + t.teamName);
+ }
+ int pointsA;
+ int pointsB;
+ int winsA = 0;
+ int winsB = 0;
+ public String playGame(Team a, Team b){
+   for(int i=0;i<50;i++){
+    pointsA = 0;
+    pointsB = 0;
+    if(a.wins - a.losses > b.wins - b.losses) pointsA += 30;
+    else pointsB += 30;
+    if(a.sosPythRank > b.sosPythRank) pointsA += 20;
+    else pointsB += 20;
+    if(a.adjORank + a.adjDRank < b.adjORank + b.adjDRank) pointsA += 20;
+    else pointsB += 20;
+    if(a.oppORank + a.oppDRank < b.oppORank + b.oppDRank) pointsA += 20;
+    else pointsB += 20;
+    for(int o;o<a.adjT;o++){
+     pointsA += (int)(Math.random()*4);
+    }
+    for(int d;d<b.adjT;d++){
+     pointsB += (int)(Math.random()*4);
+    }
+    if(pointsA > pointsB) winsA++;
+     else winsB++;
+   }
+   if(winsA > winsB)
+     return(a.teamName);
+    else return(b.teamName);
+ }
+}
---
+++ attachments/Team.java
@@ -0,0 +1,30 @@
+public class Team{
+ int rank, wins, losses;
+ String teamName;
+ double pyth, adjO, adjORank, adjD, adjDRank, adjT, adjTRank, luckRank, sosPyth, sosPythRank, oppO, oppORank, oppD, oppDRank, ncsosPyth, ncsosPythRank;
+ 
+ 
+ 
+ public void Team(int r,String tN,int wins,int losses,double pyth,double adjO,double adjORank,double adjD,double adjDRank,double adjT,double adjTRank,double luckRank,double sosPyth,double sosPythRank,double oppO,double oppORank,double oppD,double oppDRank,double ncsosPyth,double ncsosPythRank){
+  rank = r; 
+  teamName = tN;
+  wins = wins;
+  losses = losses;
+  pyth = pyth;
+  adjO = adjO;
+  adjORank = adjORank;
+  adjD = adjD;
+  adjDRank = adjDRank;
+  adjT = adjT;
+  adjTRank = adjTRank;
+  luckRank = luckRank;
+  sosPyth = sosPyth;
+  sosPythRank = sosPythRank;
+  oppO = oppO;
+  oppORank = oppORank;
+  oppD = oppD;
+  oppDRank = oppDRank;
+  ncsosPyth = ncsosPyth;
+  ncsosPythRank = ncsosPythRank;
+ }
+}
